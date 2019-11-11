b7754976147d4097970a0bd7b81ce3e33431f202
Natalie Galligan <nat.galligan@gmail.com>
2019-11-11 20:58:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -83,7 +83,8 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   pp source
-# movies_with_director_key
+
+  pp movies_with_director_key
 
 end
 
